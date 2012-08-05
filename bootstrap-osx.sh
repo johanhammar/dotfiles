355b366cc374c3480d9cacc84a2ab503a0d04028
Johan Hammar <johan.hammar@gmail.com>
2012-08-05 23:14:23 +0200

    Symlink git config

--- bootstrap-osx.sh
+++ bootstrap-osx.sh
@@ -3,5 +3,8 @@
 ln -si ~/Development/dotfiles/bash/bashrc ~/.bashrc
 ln -si ~/Development/dotfiles/bash/profile ~/.profile
 
+ln -si ~/Development/dotfiles/git/gitconfig ~/.gitconfig
+ln -si ~/Development/dotfiles/git/gitignore ~/.gitignore
+
 ./install-osx-dev.sh
 ./install-osx-tools.sh
