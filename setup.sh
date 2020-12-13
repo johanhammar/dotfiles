8dd9f2120b927d52f47bc6d76898562306c2033d
Johan Hammar <johan.hammar@gmail.com>
2020-12-13 20:34:15 +0100

    Housekeeping

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-brew install maven node scala sbt leiningen erlang elixir nmap ack git curl wget httpie
-
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-e
