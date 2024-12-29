fc57369a0f37398706d98792ed0a191aa1e2f4ae
Johan Hammar <johan.hammar@gmail.com>
2024-12-29 17:54:00 +0100

    Improve keymapping documentation

--- nvim/.config/nvim/lua/johanh/remap.lua
+++ nvim/.config/nvim/lua/johanh/remap.lua
@@ -1,19 +1,25 @@
 vim.g.mapleader = ","
 
+-- Move selected lines up/down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
+
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Find next / previous
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- next greatest remap ever : asbjornHaland
+-- Copy the selection to the system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+
+-- Copy the whole line to the system clipboard
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
@@ -21,6 +27,7 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 -- Ctrl-c exits insert 
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
+-- Clear search
 vim.keymap.set("n", "<leader><space>", vim.cmd.nohlsearch)
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
