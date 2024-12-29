03d393b116bd830b627a4124a6e88da7e2044c33
Johan Hammar <johan.hammar@gmail.com>
2024-12-29 17:48:06 +0100

    Migrate to Harpoon2
    Add a few more keymappings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -32,7 +32,13 @@ require('packer').startup(function(use)
 
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('nvim-treesitter/playground')
-    use('theprimeagen/harpoon')
+
+    use {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        requires = { { "nvim-lua/plenary.nvim" } }
+    }
+
     use('mbbill/undotree')
 
     use('nvim-lualine/lualine.nvim')
